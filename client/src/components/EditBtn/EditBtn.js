4f39a0f79877ee00cb73af1da623c365459b9be5
copiousamounts <nick.hernstedt@gmail.com>
2020-05-20 13:55:16 -0700

    added on click function to button

--- client/src/components/EditBtn/EditBtn.js
+++ client/src/components/EditBtn/EditBtn.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 function EditBtn() {
+  function editBug() {
+    alert("Hello!");
+  }
+
   return (
-    <div>
-      <i
-        className="fa fa-edit"
-        style={{ marginLeft: "280px", transform: "translateY(-25px)" }}
-      ></i>
-    </div>
+    <i
+      onClick={editBug}
+      className="fa fa-edit"
+      style={{ marginLeft: "280px", transform: "translateY(-25px)" }}
+    ></i>
   );
 }
 
