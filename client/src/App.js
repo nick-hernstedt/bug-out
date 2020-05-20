c87c50a5e4d1a19e4c40977434e81cf8f4dc73d6
copiousamounts <nick.hernstedt@gmail.com>
2020-05-20 13:17:51 -0700

    created bug card basic layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from "react";
 import "./App.css";
-import BugBox from "./components/BugBox/BugBox"
-import Nav from "./components/Nav/Nav"
-import InProgress from "./components/InProgress/InProgress"
-import Completed from "./components/Completed/Completed"
-import NeedHelp from "./components/NeedHelp/NeedHelp"
+import BugBox from "./components/BugBox/BugBox";
+import Nav from "./components/Nav/Nav";
+import InProgress from "./components/InProgress/InProgress";
+import Completed from "./components/Completed/Completed";
+import NeedHelp from "./components/NeedHelp/NeedHelp";
+import BugCards from "./components/BugCards/BugCards";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Nav />
-          <BugBox></BugBox>
-          <InProgress></InProgress>
-          <NeedHelp></NeedHelp>
-          <Completed></Completed>
-        
+        <BugBox>
+          <BugCards></BugCards>
+        </BugBox>
+        <InProgress></InProgress>
+        <NeedHelp></NeedHelp>
+        <Completed></Completed>
       </div>
-      
-    )
-}
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/BugCards/BugCards.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BugCards() {
+  return (
+    <div style={{ height: "30px", width: "200px" }}>
+      <p>Here is an example bug</p>
+    </div>
+  );
+}
+
+export default BugCards;
