d4cab0451d2de7f2b2485b84e21e61c25f40b390
copiousamounts <nick.hernstedt@gmail.com>
2020-05-21 19:08:08 -0700

    created basic bug details component

---
+++ client/src/components/BugDetails/BugDetails.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function BugDetails() {
+  return (
+    <div>
+      <form>
+        <label for="fname">Bug Details:</label>
+        <br />
+        <input type="text" name="name" />
+      </form>
+    </div>
+  );
+}
+
+export default BugDetails;
