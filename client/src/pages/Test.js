ef9fcdda44812ca79c60972e21db6ae8e525c3b6
Drubaloo <Drubaloo@outlook.com>
2020-05-21 21:17:26 -0700

    fixed test page

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -1,7 +1,20 @@
 import React from "react"
+import BugBox from "./components/BugBox/BugBox";
+import Nav from "./components/Nav/Nav";
+import InProgress from "./components/InProgress/InProgress";
+import Completed from "./components/Completed/Completed";
+import NeedHelp from "./components/NeedHelp/NeedHelp";
 
 function Test() {
-    return <h1>Sign up go here</h1>
+    return (
+        <div className="container">
+            <Nav/>
+            <BugBox></BugBox>
+            <InProgress></InProgress>
+            <Completed></Completed>
+            <NeedHelp></NeedHelp>
+        </div>
+    )
 }
 
 export default Test
