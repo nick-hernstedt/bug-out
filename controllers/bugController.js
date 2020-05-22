ad718f930338bdf136cd398620efd99b30e2949d
Drubaloo <Drubaloo@outlook.com>
2020-05-21 20:03:00 -0700

    routes are working

--- controllers/bugController.js
+++ controllers/bugController.js
@@ -1,10 +1,9 @@
 const db = require("../models");
 
-// Defining methods for the booksController
 module.exports = {
   findAll: function(req, res) {
     db.Bug
-      .find(req.query)
+      .find()
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
