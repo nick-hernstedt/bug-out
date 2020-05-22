5e5be287ca7eca1910cad8d19567801124904c70
Drubaloo <Drubaloo@outlook.com>
2020-05-21 21:19:34 -0700

    fixed test page

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -1,9 +1,9 @@
 import React from "react"
-import BugBox from "./components/BugBox/BugBox";
-import Nav from "./components/Nav/Nav";
-import InProgress from "./components/InProgress/InProgress";
-import Completed from "./components/Completed/Completed";
-import NeedHelp from "./components/NeedHelp/NeedHelp";
+import BugBox from "../components/BugBox/BugBox";
+import Nav from "../components/Nav/Nav";
+import InProgress from "../components/InProgress/InProgress";
+import Completed from "../components/Completed/Completed";
+import NeedHelp from "../components/NeedHelp/NeedHelp";
 
 function Test() {
     return (
