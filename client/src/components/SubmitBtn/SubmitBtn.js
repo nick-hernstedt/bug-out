b1892d55bab2cf09af3fc30d4eabcea6cb7e400d
copiousamounts <nick.hernstedt@gmail.com>
2020-06-02 20:31:14 -0700

    moved submit bug button on create bug modal

--- client/src/components/SubmitBtn/SubmitBtn.js
+++ client/src/components/SubmitBtn/SubmitBtn.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./SubmitBtn.css";
 
 function SubmitBtn() {
   return (
-    <div>
+    <div className="submitBtn">
       <button type="button">Submit</button>
     </div>
   );
