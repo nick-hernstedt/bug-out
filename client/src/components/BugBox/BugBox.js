6b8c52cf3c0dede025a21efcc080647b468d100d
Kayla Ward <kaylaw0127@gmail.com>
2020-06-04 20:33:25 -0700

    fixed divs in bugbox

--- client/src/components/BugBox/BugBox.js
+++ client/src/components/BugBox/BugBox.js
@@ -1,13 +1,11 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
 
 function BugBox({ children }) {
   return (
     <div>
-      <div>
-        <h4 className="header">Bugs to Solve</h4>
-      </div>
-      <div className="card-body">{children}</div>
+      <h4 className="header">Bugs to Solve</h4>
+      <div>{children}</div>
     </div>
   );
 }
