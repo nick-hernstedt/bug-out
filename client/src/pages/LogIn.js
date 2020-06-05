272cefc6575dd725070afb20ad2ac64a526b4c6b
copiousamounts <nick.hernstedt@gmail.com>
2020-06-04 18:52:57 -0700

    styled signup page

--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,62 +1,83 @@
 import React, { useState, useEffect } from "react";
-import API from "../utils/API"
-import axios from "axios"
+import API from "../utils/API";
+import axios from "axios";
+import "./app.css";
 
 function LogIn() {
-    const [signUp, setSignUp] = useState({
-        email: "",
-        password: "",
-    });
+  const [signUp, setSignUp] = useState({
+    email: "",
+    password: "",
+  });
 
-    function handleChange(event) {
-        const { name, value } = event.target;
-        setSignUp({ ...signUp, [name]: value });
-    }
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setSignUp({ ...signUp, [name]: value });
+  }
 
-    function onSubmit(event) {
-        event.preventDefault()
+  function onSubmit(event) {
+    event.preventDefault();
 
-        console.log(signUp.email)
+    console.log(signUp.email);
 
-        axios.post("/api/users/login", {
-            user: {
-                email: signUp.email,
-                password: signUp.password
-            }
-        }).then(function (response) {
-            window.location.replace("/test")
-        }).catch(function (err) {
-            alert("incorrect password or project name")
-        })
+    axios
+      .post("/api/users/login", {
+        user: {
+          email: signUp.email,
+          password: signUp.password,
+        },
+      })
+      .then(function (response) {
+        window.location.replace("/test");
+      })
+      .catch(function (err) {
+        alert("incorrect password or project name");
+      });
+  }
 
-    }
-
-
-        return (
-            <div>
-                <div className="title">
-                    <h1>Her de der</h1>
-                </div>
-                <form className="signup">
-                    <div className="form-group">
-                        <label for="exampleInputEmail1">Project name</label>
-                        <input className="form-control" id="email-input" placeholder="Project Name" name="email" onChange={handleChange} />
-                    </div>
-                    <div className="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" className="form-control" id="password-input" placeholder="Password" name="password" onChange={handleChange} />
-                    </div>
-                    <div id="alert" className="alert alert-danger" role="alert">
-                        <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-                        <span className="sr-only">Error:</span> <span className="msg"></span>
-                    </div>
-                    <button type="submit" onClick={(event) => onSubmit(event)} className="btn btn-default">Sign Up</button>
-                </form>
-            </div>
-
-        );
-    
+  return (
+    <div className="container">
+      <div>
+        <h1 className="title">B U G - O U T</h1>
+      </div>
+      <form className="signup">
+        <div className="form-group">
+          <label for="exampleInputEmail1">Project name:</label>
+          <input
+            className="form-control"
+            id="email-input"
+            placeholder="Project Name"
+            name="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label for="exampleInputPassword1">Password:</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password-input"
+            placeholder="Password"
+            name="password"
+            onChange={handleChange}
+          />
+        </div>
+        <div id="alert" className="alert alert-danger d-none" role="alert">
+          <span
+            className="glyphicon glyphicon-exclamation-sign"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Error:</span> <span className="msg"></span>
+        </div>
+        <button
+          type="submit"
+          onClick={(event) => onSubmit(event)}
+          className="signupBtn"
+        >
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
 }
 
-
-    export default LogIn
+export default LogIn;
