9ac292fcb103ae2042588683afe7456c56124b3b
Drubaloo <Drubaloo@outlook.com>
2020-06-13 14:21:56 -0700

    fixed token storage

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -28,8 +28,7 @@ function SignUp() {
       })
       .then(function (response) {
         localStorage.setItem("data", JSON.stringify(response.data));
-        localStorage.setItem("token", JSON.stringify(response.data.token));
-        console.log(JSON.stringify(response.data.user));
+        localStorage.setItem("token", JSON.stringify(response.data.user.token));
         window.location.replace("/test");
       })
       .catch(function (err) {
